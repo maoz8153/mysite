d4a7c436523816c10aee91f697f737b84416ed33
asafoutbrain <asaf@outbrain.com>
2014-04-25 21:57:00 +0300

    new design

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20140325100635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "adminpack"
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
